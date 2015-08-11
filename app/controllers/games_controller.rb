8d9ce29044a84ead920e0d28361f09ebba5d4ecc
Chris Elwood <kudzooman@gmail.com>
2015-08-11 12:40:57 -0400

    modified index, new and edit game views

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,8 +8,33 @@ class GamesController < ApplicationController
   end
 
   def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(params.require(:game).permit(:game_number, :favorite, :spread, :underdog, :favorite_score, :underdog_score))
+    if @game.save
+      flash[:notice] = "Game has been saved."
+      redirect_to @game
+    else
+      flash[:error] = "Problem."
+      render :new
+    end
   end
 
   def edit
+    @game = Game.find(params[:id])
   end
+
+  def update
+    @game = Game.find(params[:id])
+    if @game.update_attributes(params.require(:game).permit(:game_number, :favorite, :spread, :underdog, :favorite_score, :underdog_score))
+      flash[:notice] = "Game updated"
+      redirect_to @game
+    else
+      flash[:error] = "There was a problem"
+      render :edit
+    end
+  end
+
 end
